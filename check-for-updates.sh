2a7518299b726c4979bb9986114e8c9b4be63359
Daniel Rudolf <github.com@daniel-rudolf.de>
2025-09-27 18:12:16 +0200

    Add GitHub workflow to auto update (rebuild & publish) containers

---
+++ check-for-updates.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Unbound
+# A container running Butane, a config translator for FCOS' Ignition.
+#
+# Copyright (c) 2025  SGS Serious Gaming & Simulations GmbH
+#
+# This work is licensed under the terms of the MIT license.
+# For a copy, see LICENSE file or <https://opensource.org/licenses/MIT>.
+#
+# SPDX-License-Identifier: MIT
+# License-Filename: LICENSE
+
+set -eu -o pipefail
+export LC_ALL=C.UTF-8
+
+[ -v CI_TOOLS ] && [ "$CI_TOOLS" == "SGSGermany" ] \
+    || { echo "Invalid build environment: Environment variable 'CI_TOOLS' not set or invalid" >&2; exit 1; }
+
+[ -v CI_TOOLS_PATH ] && [ -d "$CI_TOOLS_PATH" ] \
+    || { echo "Invalid build environment: Environment variable 'CI_TOOLS_PATH' not set or invalid" >&2; exit 1; }
+
+source "$CI_TOOLS_PATH/helper/common.sh.inc"
+source "$CI_TOOLS_PATH/helper/chkupd.sh.inc"
+source "$CI_TOOLS_PATH/helper/github.sh.inc"
+
+BUILD_DIR="$(CDPATH= cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+source "$BUILD_DIR/container.env"
+
+TAG="${TAGS%% *}"
+
+# check whether the base image was updated
+chkupd_baseimage "$REGISTRY/$OWNER/$IMAGE" "$TAG" || exit 0
+
+# check whether a different Git tag was marked as latest GitHub release
+GITHUB_REPO_IDENT="${BUTANE_GIT_REPO#https://github.com/}"
+GITHUB_LATEST_VERSION="$(github_latest "$GITHUB_REPO_IDENT")"
+
+chkupd_image_version "$REGISTRY/$OWNER/$IMAGE:$TAG" \
+    "${GITHUB_LATEST_VERSION#v}" \
+    || exit 0
